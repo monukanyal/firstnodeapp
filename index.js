c82ec5914ef0a586d65580dadd0f8823e27a6692
monukanyal <monu_kanyal@esferasoft.com>
2017-07-01 19:56:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+                        var express = require('express');
 			var mysql=require('mysql');
 			var md5 = require('md5');
 			var session = require('express-session')			
@@ -10,19 +10,18 @@ var express = require('express');
 			var nodemailer=require('nodemailer');
 			var http=require('http').Server(app);
 			var io=require('socket.io')(http);
-     app.set('port', (process.env.PORT || 5000));
-   http.listen(app.get('port'));
+                       app.set('port', (process.env.PORT || 5000));
+                       http.listen(app.get('port'));
 
 app.use(upload()); //for file upload -- html form file upload imp
-app.use('/public', express.static('vendors'));  // for static js ,css, image file path --use public/
-//app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  console.log("Node app is running at localhost:" + app.get('port'));
-  //response.send('Welcome Monu kanyal!');
-  response.render('index',{page_title:'Home'}); 
+ 
+       //response.send('Welcome Monu kanyal!');
+       response.render('index',{page_title:'Home'}); 
 })
 
-// app.listen(app.get('port'), function() {
-//   console.log("Node app is running at localhost:" + app.get('port'))
-// })
+ app.listen(app.get('port'), function() {
+	 console.log("Node app is running at localhost:" + app.get('port'))
+ })
