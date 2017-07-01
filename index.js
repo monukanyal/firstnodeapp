443dcd49370ba62a2000bec5fd4efcb8368f9e63
MONU KANYAL <MONU KANYAL>
2017-07-01 19:46:18 +0530

    all updates

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ var express = require('express');
      app.set('port', (process.env.PORT || 5000));
    http.listen(app.get('port'));
 
-
+app.use(upload()); //for file upload -- html form file upload imp
+app.use('/public', express.static('vendors'));  // for static js ,css, image file path --use public/
 //app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   console.log("Node app is running at localhost:" + app.get('port'));
-  response.send('Welcome Monu kanyal!');
+  //response.send('Welcome Monu kanyal!');
+  response.render('index',{page_title:'Home'}); 
 })
 
 // app.listen(app.get('port'), function() {
