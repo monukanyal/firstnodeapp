ac0ccc346b89954f234d9415f8a72703edd9b5d3
monukanyal <monu_kanyal@esferasoft.com>
2017-07-01 18:26:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+			var mysql=require('mysql');
+			var md5 = require('md5');
+			var session = require('express-session')			
+			var app  = express();
+			
+			app.set('view engine', 'ejs');
 
-app.set('port', (process.env.PORT || 5000))
+			var upload=require('express-fileupload');
+			var nodemailer=require('nodemailer');
+			var http=require('http').Server(app);
+			var io=require('socket.io')(http);
+     app.set('port', (process.env.PORT || 5000));
+   http.listen(app.get('port'));
+
+
 //app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
+  console.log("Node app is running at localhost:" + app.get('port'));
   response.send('Welcome Esferasoft!')
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+// app.listen(app.get('port'), function() {
+//   console.log("Node app is running at localhost:" + app.get('port'))
+// })
